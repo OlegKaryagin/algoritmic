ef5bace7965ea4926b6f096c7f6465c3e7656d09
OlegKaryagin <63190353+OlegKaryagin@users.noreply.github.com>
2020-10-07 16:44:25 +0300

    Pipe
    Structure of pipe (Error Rus simbol)

---
+++ LabPipe/LabPipe/LabPipe.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+struct pipe
+{
+	int length;
+	int diametr;
+	std::string status;
+};
+
+
+
+int main()
+{
+	pipe p1;
+	p1.length = 12;
+	p1.diametr = 5;
+	p1.status = "В ремонте";
+	std::cout << "Длина:" << p1.length << "Диаметр" << p1.diametr << "Статус" << p1.status << std::endl;
+	return 0;
+}
