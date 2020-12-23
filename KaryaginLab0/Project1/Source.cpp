f33bc8b1d8a2ba0cb1a5b1ca278b37449b84705b
OlegKaryagin <63190353+OlegKaryagin@users.noreply.github.com>
2020-12-23 13:33:51 +0300

    защита

--- KaryaginLab0/Project1/Source.cpp
+++ KaryaginLab0/Project1/Source.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void f(vector<int>& v)
+{
+	int n = 5;
+	v.resize(n);
+}
 
 int main() {
+	vector<int> vec;
+	f(vec);
 	char symbol;
 	cout << "Enter the symbol"<<endl;
 	cin >> symbol;
